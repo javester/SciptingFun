1247941f8022f102f69052de8b6b83c4d9309fad
Javan <javester@users.noreply.github.com>
2020-06-04 13:32:16 -0700

    Update Get-BlobTypeTotalSize.ps1

--- Get-BlobTypeTotalSize.ps1
+++ Get-BlobTypeTotalSize.ps1
@@ -1,42 +1,78 @@
 <#
 # get total size of VHD files (or whatever file filter you want)  found in storage account
   might also count up snapshots sizes? let me know
-  6/4/2020 v1
+
+  ARM vs CLASSIC:
+   
+  ARM accounts require the resource group param
+
 #>
 
-
 param(
-$resourcegroupname = 'resourcegroupname',    # PROVIDE RESOURCE GROUP OF STORAGE ACCOUNT
-$storageaccountname = 'storageaccountname',  # PROVIDE STORAGE ACCOUNT NAME
+$resourcegroupname = 'resourcegroup',          # PROVIDE RESOURCE GROUP OF STORAGE ACCOUNT if ARM
+$storageaccountname = 'storageaccount',        # PROVIDE STORAGE ACCOUNT NAME rg18579 
 [switch]$nototal = $true
 )
 
 $output = @()
 $totalsizeofVHDs = 0
 
-$stc = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -ErrorAction Stop
+#ARM
+try{$stc = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -ErrorAction Stop}
+catch{$sanotfound=$true}
 
-$containers = Get-AzStorageContainer -Context $stc.Context -ErrorAction Stop
-
-foreach ($container in $containers)
+if ($sanotfound -ne $true)
 {
-    $blobs = Get-AzStorageBlob -Name $container.name -Context $stc.Context -ErrorAction Stop
+    $containers = Get-AzStorageContainer -Context $stc.Context -ErrorAction Stop
 
-    foreach ($blob in $blobs)
+    foreach ($container in $containers)
     {
-        if ($blob.Name -like '*.vhd' -and $blob.IsDeleted -eq $false) # FILTER TO WHATEVER YOU WANT
-        {   $o = New-Object -TypeName psobject
-            $o | Add-Member -MemberType NoteProperty -Name Name -Value $blob.Name
-            $o | Add-Member -MemberType NoteProperty -Name StorageAccount -Value $storageAccountName
-            $o | Add-Member -MemberType NoteProperty -Name Container -Value $container.Name
-            $o | Add-Member -MemberType NoteProperty -Name SizeGB -Value $null
-            $size = ([Math]::Round($($blob.Length)/1gb,3))
-            $o.SizeGB = $size
-            $totalsizeofVHDs += $size
-            $output += $o
+        $blobs = Get-AzStorageBlob -Name $container.name -Context $stc.Context -ErrorAction Stop
+
+        foreach ($blob in $blobs)
+        {
+            if ($blob.Name -like '*.vhd' -and $blob.IsDeleted -eq $false) # FILTER TO WHATEVER YOU WANT
+            {   $o = New-Object -TypeName psobject
+                $o | Add-Member -MemberType NoteProperty -Name Name -Value $blob.Name
+                $o | Add-Member -MemberType NoteProperty -Name StorageAccount -Value $storageAccountName
+                $o | Add-Member -MemberType NoteProperty -Name Container -Value $container.Name
+                $o | Add-Member -MemberType NoteProperty -Name SizeGB -Value $null
+                $size = ([Math]::Round($($blob.Length)/1gb,3))
+                $o.SizeGB = $size
+                $totalsizeofVHDs += $size
+                $output += $o
+            }
         }
     }
 }
+
+else
+{
+    #CLASSIC 
+    $stc = (Get-AzureStorageAccount -StorageAccountName $storageaccountname).Context
+    $containers = Get-AzureStorageContainer -Context $stc -ErrorAction Stop
+    foreach ($container in $containers)
+    {
+        $blobs = Get-AzureStorageBlob -Name $container.name -Context $stc.Context -ErrorAction Stop
+
+        foreach ($blob in $blobs)
+        {
+            if ($blob.Name -like '*.vhd' -and $blob.IsDeleted -eq $false) # FILTER TO WHATEVER YOU WANT
+            {   $o = New-Object -TypeName psobject
+                $o | Add-Member -MemberType NoteProperty -Name Name -Value $blob.Name
+                $o | Add-Member -MemberType NoteProperty -Name StorageAccount -Value $storageAccountName
+                $o | Add-Member -MemberType NoteProperty -Name Container -Value $container.Name
+                $o | Add-Member -MemberType NoteProperty -Name SizeGB -Value $null
+                $size = ([Math]::Round($($blob.Length)/1gb,3))
+                $o.SizeGB = $size
+                $totalsizeofVHDs += $size
+                $output += $o
+            }
+        }
+    }
+
+}
+
 
 $output
 
