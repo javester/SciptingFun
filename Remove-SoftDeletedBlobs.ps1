bcac1df5314f3f414c631c2aac424c5cfbac457c
Javan <javester@users.noreply.github.com>
2020-07-04 13:34:59 -0700

    Update Remove-SoftDeletedBlobs.ps1

--- Remove-SoftDeletedBlobs.ps1
+++ Remove-SoftDeletedBlobs.ps1
@@ -1,20 +1,26 @@
 #
-# This script is used to clean up soft deleted blobs that are no longer needed.
-# It will search a specified Azure Storage Account container for deleted blobs, undelete them, then delete them.
+# This script is used to clean up soft deleted blobs and snapshots that are no longer needed.
+# It will search a specified Azure Storage Account container for deleted blobs and snapshots, undelete them, then delete them.
 # Before running this script, make sure that Soft Delete is DISABLED on the storage account, or else the deletion operation will simply put them right back into the soft deleted state they were in before.
 #
 #
 # v1.0 07/04/2020
 #
 
-Write-Host "`nPlease make sure that soft-delete is disabled on the storage account before executing!"
 
-# Provide variables below
-$resourceGroupName = '<RG Name>'
-$storageaccountname = '<SA Name>'
-$ContainerName = '<Container Name>'
+param(
+
+$resourceGroupName = '<rg>',
+$storageaccountname = '<sa>',
+$ContainerName = '<cntr>'
+
+)
+
 
 # --------------------------------------------------------------------
+
+Write-Host "`nPlease make sure that soft-delete is disabled on the storage account before executing!"
+
 $ctx = Get-AzStorageAccount -name $storageaccountname -resourcegroup $resourceGroupName -ErrorAction Stop
 $blobs = Get-AzStorageBlob -IncludeDeleted -Context $ctx.Context -Container $ContainerName -ErrorAction Stop |?{$_.IsDeleted -eq $true}
 
@@ -32,7 +38,17 @@ foreach ($delblob in $blobs)
     $blobsize = ($($delblob.Length/1MB))
     
     $i++
-    Write-Output "$i / $($blobs.count): Deleting Soft-Deleted Blob: $($delblob.Name) $([math]::Round($blobsize,3))MB $($delblob.LastModified)"
+    $snap=$false
+    
+    if ($delblob.SnapshotTime -ne $null)
+    {
+        $snap = $true
+    }
+    if (!$snap)
+    {Write-Output "$i / $($blobs.count): Deleting Soft-Deleted Blob: $($delblob.Name) $([math]::Round($blobsize,3))MB $($delblob.LastModified)"}
+    else
+    {Write-Output "$i / $($blobs.count): Deleting Soft-Deleted Snapshot: $($delblob.Name) $([math]::Round($blobsize,3))MB $($delblob.SnapshotTime)"}
+
     $delblob.ICloudBlob.Undelete()
     $delblob | Remove-AzStorageBlob -Force
 }
